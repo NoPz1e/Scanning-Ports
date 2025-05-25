bbeac16ec6e8d618296d823a1c5c9b25c15dee91
Cristiano Neto <Cristianomanuelneto2006@gmail.com>
2025-05-25 22:40:09 +0100

    Add Comments

--- portscan1.py
+++ portscan1.py
@@ -4,13 +4,19 @@ import socket
 
 sk = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+# SAVE IP and port that I want to scan
 host = "194.168.1.73"
 port = 443
 
+# Function responsible for the scan
 def portscanner(port):
+
+	# Condition checks if it is possible to connect to the specified port
+	# connect_ex, if it fails, returns that the port is closed.
 	if sk.connect_ex((host,port)):
 		print("Port %d is closed" % (port))
 	else:
 		print("Port %d is open" % (port))
 
+# Call function "portscanner"
 portscanner(port)
